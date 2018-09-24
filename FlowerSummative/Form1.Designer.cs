ea7fd7f61c0c8bcfef42f001a06d3a4c371e5bad
Conall Gillett <conagill060@ed.amdsb.ca>
2018-09-24 14:40:42 -0400

    Add project files.

---
+++ FlowerSummative/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace FlowerSummative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(400, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "YOUR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(233, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 55);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "IT\'S";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(260, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(267, 55);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "BIRTHDAY";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(297, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 87);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Push for Gift";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FlowerSummative/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlowerSummative
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            button1.Visible = false;
+            int x = 10;
+            int y = 190;
+
+            Graphics g = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Brown, 10);
+            SolidBrush drawbrush = new SolidBrush(Color.Yellow);
+
+            while (x < 370)
+            {
+                g.Clear(Color.White);
+                g.DrawRectangle(drawPen, x, 190, 5, 5);
+                x = x + 15;
+                g.DrawRectangle(drawPen, x, 200, 5, 5);
+                x = x + 15;
+                g.DrawRectangle(drawPen, x, 190, 5, 5);
+                x = x + 15;
+                Thread.Sleep(500);
+
+            }
+
+            drawPen.Color = Color.Green;
+            x = x - 30;
+            while (y > 120)
+            {
+                g.DrawRectangle(drawPen, x, y, 5, 5);
+                y = y - 5;
+                Thread.Sleep(500);
+
+            }
+            g.FillEllipse(drawbrush, x - 10, y - 10, 20, 25);
+            Thread.Sleep(500);
+            drawbrush.Color = Color.Red;
+            g.FillEllipse(drawbrush, x - 10, y - 40, 20, 40);
+            Thread.Sleep(500);
+            g.TranslateTransform(x + 15, y - 345);
+            g.RotateTransform(80);
+            g.FillEllipse(drawbrush, x - 8, y - 40, 20, 40);
+            g.ResetTransform();
+            Thread.Sleep(500);
+            g.TranslateTransform(x - 3, y - 365);
+            g.RotateTransform(75);
+            g.FillEllipse(drawbrush, x + 15, y - 75, 20, 40);
+            g.ResetTransform();
+            Thread.Sleep(500);
+            Graphics z = this.CreateGraphics();
+            Font drawFont = new Font("Arial", 36, FontStyle.Bold);
+            SolidBrush drawBrush = new SolidBrush(Color.Blue);
+            SoundPlayer party_horn = new SoundPlayer(Properties.Resources.party_horn); 
+            party_horn.Play();
+            z.DrawString("Happy", drawFont, drawBrush, 75, 50);
+            z.DrawString("Birthday", drawFont, drawBrush, 400, 50);
+        }
+    }
+    }
